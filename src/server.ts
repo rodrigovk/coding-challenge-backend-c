7db4c94889893bca72ca889bc590364946efe882
rodrigovk <rodrigocasadeialanis@hotmail.com>
2023-08-13 19:40:50 -0300

    fix: remove url from listen command

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const url = env.server.url;
 const port = env.server.port;
 
 setupApp().then((app) => {
-  app.listen(port, url, () =>
+  app.listen(port, () =>
     console.log(
       `Server running at http://${url || '127.0.0.1'}:${port}/suggestions`
     )
